ce124f76c2709e770f8a209ac8914ab7142686b5
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-06-17 20:59:24 -0300

    ajustando código base do projeto original

---
+++ app/src/main/java/com/example/telaallprojectsfilefuncional/AllProjectsFile.kt
@@ -0,0 +1,34 @@
+package com.example.module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.module.databinding.ActivityAllProjectsFileBinding
+
+class AllProjectsFile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAllProjectsFileBinding
+    private lateinit var ProjectCardAdapter: ProjectCardAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAllProjectsFileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecycleView()
+        addAllProjectsDatasource()
+    }
+
+    private fun initRecycleView() {
+        this.ProjectCardAdapter = ProjectCardAdapter()
+
+        binding.AllProjectsRecycleView.layoutManager = LinearLayoutManager(this@AllProjectsFile)
+        binding.AllProjectsRecycleView.adapter = this.ProjectCardAdapter
+    }
+
+    private fun addAllProjectsDatasource() {
+        val allProjectsSourceData = DataSource.generateAllProjectsDataset()
+        this.ProjectCardAdapter.setDataset(allProjectsSourceData)
+    }
+}
--- app/src/main/java/com/example/telaallprojectsfilefuncional/MainActivity.kt
+++ app/src/main/java/com/example/telaallprojectsfilefuncional/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.telaallprojectsfilefuncional
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.module.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
